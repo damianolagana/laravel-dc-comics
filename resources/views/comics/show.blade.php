b1cc6cd77195428a6456e5388772748692ce078a
damianolagana <damianolagana@gmail.com>
2023-07-06 11:43:38 +0200

    single book style fixed

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -3,16 +3,26 @@
 @section('content')
 <div class="container my-3">
     <h1>Dettaglio Comic</h1>
-    <div class="row g-4">
+    <div >
         <div>
             <a href="{{route("comics.index")}}">Torna alla Comics List</a>
         </div>
-        <div class="w-50">
-            <h1 class="text-center">{{$comics->title}}</h4>
-            <div class="text-center">
-                <img class="w-75" src="{{$comics->thumb}}" alt="">
+        <div class="d-flex pt-5">
+            <div class="w-50">
+                <h1 class="text-center">{{$comics->title}}</h4>
+                <div class="text-center">
+                    <img class="w-75" src="{{$comics->thumb}}" alt="">
+                </div>
+                <p class="text-center">{{$comics->series}}</p>
             </div>
-            <p class="text-center">{{$comics->series}}</p>
+            <div class="w-50">
+                <h3>Description</h3>
+                <p>{{$comics->description}}</p>
+                <p>Book Type: {{$comics->type}}</p>
+                <p>Price: ${{$comics->price}}</p>
+                <p>Sale Date: {{$comics->sale_date}}</p>
+            </div>
+
         </div>
        
     </div>
